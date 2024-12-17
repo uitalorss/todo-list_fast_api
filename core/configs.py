5b7d8e328a3151a804479091663eb24cb110169f
Uítalo Souza <uitalo.urss@gmail.com>
2024-12-17 09:16:51 -0300

    feat: criação da camada core do projeto

---
+++ core/configs.py
@@ -0,0 +1,21 @@
+from typing import ClassVar
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+class Settings(BaseSettings):
+    API_V1_STR: ClassVar[str] = "api/v1"
+    DB_URL = ClassVar[str] = "sqlite+aiosqlite:///./dev.db"
+    DBBaseModel: ClassVar[DeclarativeMeta] = declarative_base()
+
+    JWT_KEY: str = os.getenv("JWT_KEY")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 8
+
+    class Config:
+        case_sensitive: True
+
+settings: Settings = Settings()
